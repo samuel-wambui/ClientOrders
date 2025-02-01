0b738692a83c0d7ee690f1c4d21d97b74364a14f
samuel-wambui <147292274+samuel-wambui@users.noreply.github.com>
2025-02-02 00:10:07 +0300

    white noice error debuggig

--- ClientOrders/ClientOrders/wsgi.py
+++ ClientOrders/ClientOrders/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ClientOrders.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ClientOrders.ClientOrders.settings')
 
 application = get_wsgi_application()
 from whitenoise import WhiteNoise
