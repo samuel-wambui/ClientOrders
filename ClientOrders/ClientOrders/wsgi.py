a793d2c56352a037449871d760ea6915044cf227
samuel-wambui <147292274+samuel-wambui@users.noreply.github.com>
2025-02-02 02:22:19 +0300

    white noice error debuggig

--- ClientOrders/ClientOrders/wsgi.py
+++ ClientOrders/ClientOrders/wsgi.py
@@ -8,11 +8,16 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Get the directory one level above ClientOrders/
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Add BASE_DIR to sys.path
+sys.path.insert(0, BASE_DIR)
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ClientOrders.settings')
 
 application = get_wsgi_application()
-from whitenoise import WhiteNoise
-application = WhiteNoise(application)
