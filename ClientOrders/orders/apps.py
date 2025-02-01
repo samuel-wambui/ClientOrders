2d3c9d44231df93bd8d37ab6dad0b2005eb48447
samuel-wambui <147292274+samuel-wambui@users.noreply.github.com>
2025-02-02 01:59:27 +0300

    white noice error debuggig

--- ClientOrders/orders/apps.py
+++ ClientOrders/orders/apps.py
@@ -3,8 +3,7 @@ from django.apps import AppConfig
 
 class OrdersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ClientOrders.orders'
-
+    name = 'orders'
 
 
 
