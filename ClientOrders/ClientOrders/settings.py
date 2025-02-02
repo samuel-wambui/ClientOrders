282bcd85a114a1390a200dc0bb49df962137400f
samuel-wambui <147292274+samuel-wambui@users.noreply.github.com>
2025-02-02 22:57:21 +0300

    Add templates folder and update TEMPLATES setting

--- ClientOrders/ClientOrders/settings.py
+++ ClientOrders/ClientOrders/settings.py
@@ -99,7 +99,7 @@ LOGOUT_REDIRECT_URL = "/"
 
 # OIDC callback endpoint – must match the one registered in the Google Developer Console.
 OIDC_CALLBACK_URL = "/oidc/callback/"
-LOGIN_URL = '/oidc/authenticate/'
+LOGIN_URL = 'login'
 # settings.py
 
 LOGIN_REDIRECT_URL = '/dashboard/'
