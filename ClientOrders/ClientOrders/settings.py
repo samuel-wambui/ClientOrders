0938bab413a1da023536016609324ec6d1b390a2
samuel-wambui <147292274+samuel-wambui@users.noreply.github.com>
2025-02-02 22:27:52 +0300

    login redirect

--- ClientOrders/ClientOrders/settings.py
+++ ClientOrders/ClientOrders/settings.py
@@ -99,7 +99,7 @@ LOGOUT_REDIRECT_URL = "/"
 
 # OIDC callback endpoint – must match the one registered in the Google Developer Console.
 OIDC_CALLBACK_URL = "/oidc/callback/"
-
+LOGIN_URL = '/login/'
 
 print(os.environ.get("OIDC_CLIENT_ID"))
 print(os.environ.get("OIDC_CLIENT_SECRET"))
