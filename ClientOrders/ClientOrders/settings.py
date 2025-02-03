c366d8a54b03aaf4e9153f4ceccdffb1abeb67d3
samuel-wambui <147292274+samuel-wambui@users.noreply.github.com>
2025-02-03 12:20:34 +0300

    endpoint protection

--- ClientOrders/ClientOrders/settings.py
+++ ClientOrders/ClientOrders/settings.py
@@ -208,12 +208,14 @@ LOGGING = {
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-        'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ],
 }
 
